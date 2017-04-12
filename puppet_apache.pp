e2498fea69c07d653849c69b96186947a55f535b
swathin92 <swathin92@gmail.com>
2017-04-11 17:29:03 -0700

    new changes made to the file

--- puppet_apache.pp
+++ puppet_apache.pp
@@ -1,4 +1,14 @@
-package{apache:
+if $facts[os][family] == 'redhat' {
+    package_name = 'httpd',
+}
+else {
+    notify { 'use redhat family servers': } 
+    
+}
+    # enter puppet code
+
+
+package {package_name:
 ensure => installed,
 name => apache2,
 }
